868fbfbecee098c36ff1f66cf867233e02bf69f8
madraceee <raajanitheesh@gmail.com>
2023-08-08 22:31:48 +0530

    Initial Update

---
+++ script/Interactions.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+
+contract MintBasicNFt is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployed = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNFTOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNFT(contractAddress).mintNFT(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
